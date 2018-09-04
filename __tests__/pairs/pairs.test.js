da696591f547b8d59a931ca86300535d0e2dd457
Sergey Bakaev <bakaev@yandex.ru>
2018-09-04 21:49:59 +0300

    isPair func

--- __tests__/pairs/pairs.test.js
+++ __tests__/pairs/pairs.test.js
@@ -1,27 +1,38 @@
-import { cons, car, cdr } from '../../src/pairs/pairs';
+import {
+  cons, car, cdr, isPair,
+} from '../../src/pairs/pairs';
 
 test('Should create pair and it must be a function', () => {
   const pair = cons(1, 2);
-  expect(typeof pair).toBe('function');
-  expect(pair('qwerty')).toBe(undefined);
+  expect(isPair(pair)).toBeTruthy();
+  expect(isPair(() => {})).toBeFalsy();
+  expect(pair('qwerty')).toBeUndefined();
 });
 
 test('car should return correct value', () => {
   expect(car(cons(1, null))).toBe(1);
-  expect(car(cons(null, undefined))).toBe(null);
-  expect(car(cons(undefined, null))).toBe(undefined);
+  expect(car(cons(null, undefined))).toBeNull();
+  expect(car(cons(undefined, null))).toBeUndefined();
   expect(car(cons('1', null))).toBe('1');
   expect(car(cons(true, null))).toBe(true);
-  expect(car(cons([1, 2], null))).toEqual([1, 2]);
-  expect(car(cons({}, null))).toEqual({});
+
+  const arr = [1, 2];
+  expect(car(cons(arr, null))).toBe(arr);
+
+  const obj = { someProp: 1 };
+  expect(car(cons(obj, null))).toBe(obj);
 });
 
 test('cdr should return correct value', () => {
   expect(cdr(cons(null, 1))).toBe(1);
-  expect(cdr(cons(undefined, null))).toBe(null);
-  expect(cdr(cons(null, undefined))).toBe(undefined);
+  expect(cdr(cons(undefined, null))).toBeNull();
+  expect(cdr(cons(null, undefined))).toBeUndefined();
   expect(cdr(cons(null, '1'))).toBe('1');
   expect(cdr(cons(null, true))).toBe(true);
-  expect(cdr(cons(null, [1, 2]))).toEqual([1, 2]);
-  expect(cdr(cons(null, {}))).toEqual({});
+
+  const arr = [1, 2];
+  expect(cdr(cons(null, arr))).toBe(arr);
+
+  const obj = { someProp: 1 };
+  expect(cdr(cons(null, obj))).toBe(obj);
 });
--- src/pairs/pairs.js
+++ src/pairs/pairs.js
@@ -1,17 +1,24 @@
 const carMessage = 'car';
 const cdrMessage = 'cdr';
 
+export const cons = (a, b) => {
+  const pair = (msg) => {
+    switch (msg) {
+      case carMessage:
+        return a;
+      case cdrMessage:
+        return b;
+      default:
+        return undefined;
+    }
+  };
 
-export const cons = (a, b) => (msg) => {
-  switch (msg) {
-    case carMessage:
-      return a;
-    case cdrMessage:
-      return b;
-    default:
-      return undefined;
-  }
+  pair.pair = true;
+
+  return pair;
 };
+
+export const isPair = pair => typeof pair === 'function' && pair.pair === true;
 
 export const car = pair => pair(carMessage);
 export const cdr = pair => pair(cdrMessage);
