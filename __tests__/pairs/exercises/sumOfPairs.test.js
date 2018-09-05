563f82649852e1518811070d1d65170b39523a64
Sergey Bakaev <bakaev@yandex.ru>
2018-09-05 20:49:36 +0300

    correct import path

--- __tests__/pairs/exercises/sumOfPairs.test.js
+++ __tests__/pairs/exercises/sumOfPairs.test.js
@@ -1,6 +1,6 @@
 import { cons, toString } from '../../../src/pairs/pairs';
 
-import sumOfPairs from '../../../src/pairs/exersices/sumOfPairs';
+import sumOfPairs from '../../../src/pairs/exercises/sumOfPairs';
 
 test('sumOfPairs test', () => {
   expect(toString(sumOfPairs(cons(1, 2), cons(3, 4)))).toEqual(toString(cons(4, 6)));
