c5f80d9b6fcb7736d0ea63cbbf8c6a269c861a14
Sergey Bakaev <bakaev@yandex.ru>
2018-09-25 19:45:27 +0300

    rename steps for html builder

--- __tests__/html-builder/step1-simple-builder.test.js
+++ __tests__/html-builder/step1-simple-builder.test.js
@@ -1,4 +1,4 @@
-import { render, parse } from '../../src/html-builder/ast-builder';
+import buildHtml from '../../src/html-builder/step1-simple-builder';
 
 describe('HtmlBuilder', () => {
   it('set 1', () => {
@@ -9,14 +9,14 @@ describe('HtmlBuilder', () => {
       ['body', { class: 'container' }, [
         ['h1', { class: 'header' }, 'html builder example'],
         ['div', [
-          ['span', 'span text2'],
-          ['br'],
+          ['span'],
+          ['span', { class: 'text', id: 'uniq-key' }],
         ]],
       ]],
     ]];
 
-    const actual = render(parse(data));
-    const expected = '<html><head><title>hello, hexlet!</title></head><body class="container"><h1 class="header">html builder example</h1><div><span>span text2</span><br></div></body></html>';
+    const actual = buildHtml(data);
+    const expected = '<html><head><title>hello, hexlet!</title></head><body class="container"><h1 class="header">html builder example</h1><div><span></span><span class="text" id="uniq-key"></span></div></body></html>';
     expect(actual).toBe(expected);
   });
 
@@ -24,7 +24,6 @@ describe('HtmlBuilder', () => {
     const data = ['html', [
       ['body', [
         ['h2', { class: 'header' }, 'first header'],
-        ['img', { class: 'picture', src: 'smile.jpg' }],
         ['div', [
           ['p', 'hello, world'],
           ['p', 'good bye, world'],
@@ -33,8 +32,8 @@ describe('HtmlBuilder', () => {
       ]],
     ]];
 
-    const actual = render(parse(data));
-    const expected = '<html><body><h2 class="header">first header</h2><img class="picture" src="smile.jpg"><div><p>hello, world</p><p>good bye, world</p><a class="link" href="https://hexlet.io">hexlet.io</a></div></body></html>';
+    const actual = buildHtml(data);
+    const expected = '<html><body><h2 class="header">first header</h2><div><p>hello, world</p><p>good bye, world</p><a class="link" href="https://hexlet.io">hexlet.io</a></div></body></html>';
     expect(actual).toBe(expected);
   });
 });
--- __tests__/html-builder/step2-ast-builder.test.js
+++ __tests__/html-builder/step2-ast-builder.test.js
@@ -1,4 +1,4 @@
-import buildHtml from '../../src/html-builder/simple-builder';
+import { render, parse } from '../../src/html-builder/step2-ast-builder';
 
 describe('HtmlBuilder', () => {
   it('set 1', () => {
@@ -9,14 +9,14 @@ describe('HtmlBuilder', () => {
       ['body', { class: 'container' }, [
         ['h1', { class: 'header' }, 'html builder example'],
         ['div', [
-          ['span'],
-          ['span', { class: 'text', id: 'uniq-key' }],
+          ['span', 'span text2'],
+          ['br'],
         ]],
       ]],
     ]];
 
-    const actual = buildHtml(data);
-    const expected = '<html><head><title>hello, hexlet!</title></head><body class="container"><h1 class="header">html builder example</h1><div><span></span><span class="text" id="uniq-key"></span></div></body></html>';
+    const actual = render(parse(data));
+    const expected = '<html><head><title>hello, hexlet!</title></head><body class="container"><h1 class="header">html builder example</h1><div><span>span text2</span><br></div></body></html>';
     expect(actual).toBe(expected);
   });
 
@@ -24,6 +24,7 @@ describe('HtmlBuilder', () => {
     const data = ['html', [
       ['body', [
         ['h2', { class: 'header' }, 'first header'],
+        ['img', { class: 'picture', src: 'smile.jpg' }],
         ['div', [
           ['p', 'hello, world'],
           ['p', 'good bye, world'],
@@ -32,8 +33,8 @@ describe('HtmlBuilder', () => {
       ]],
     ]];
 
-    const actual = buildHtml(data);
-    const expected = '<html><body><h2 class="header">first header</h2><div><p>hello, world</p><p>good bye, world</p><a class="link" href="https://hexlet.io">hexlet.io</a></div></body></html>';
+    const actual = render(parse(data));
+    const expected = '<html><body><h2 class="header">first header</h2><img class="picture" src="smile.jpg"><div><p>hello, world</p><p>good bye, world</p><a class="link" href="https://hexlet.io">hexlet.io</a></div></body></html>';
     expect(actual).toBe(expected);
   });
 });
